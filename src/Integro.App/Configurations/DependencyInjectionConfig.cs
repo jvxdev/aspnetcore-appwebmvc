2a48459f6932e14c4894647bc6b527f40c2e8c08
João Victor Dias de Moraes <54956887+jvxdev@users.noreply.github.com>
2021-09-28 17:56:02 -0300

    Organização da aplicação
    Startup organizada.

---
+++ src/Integro.App/Configurations/DependencyInjectionConfig.cs
@@ -0,0 +1,23 @@
+﻿using Integro.Business.Interfaces;
+using Integro.Data.Context;
+using Integro.Data.Repository;
+using Microsoft.AspNetCore.Mvc.DataAnnotations;
+using Microsoft.Extensions.DependencyInjection;
+using static Integro.App.Extensions.MoedaAttributeAdapter;
+
+namespace Integro.App.Configurations
+{
+    public static class DependencyInjectionConfig 
+    {
+        public static IServiceCollection ResolveDependencies(this IServiceCollection services)
+        {
+            services.AddScoped<MeuDbContext>();
+            services.AddScoped<IEnderecoRepository, EnderecoRepository>();
+            services.AddScoped<IFornecedorRepository, FornecedorRepository>();
+            services.AddScoped<IProdutoRepository, ProdutoRepository>();
+            services.AddSingleton<IValidationAttributeAdapterProvider, MoedaValidationAttributeAdapterProvider>();
+
+            return services;
+        }
+    }
+}
---
+++ src/Integro.App/Configurations/MvcConfig.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Integro.App.Configurations
+{
+    public static class MvcConfig
+    {
+        public static IServiceCollection AddMvcConfiguration(this IServiceCollection services)
+        {
+            services.AddMvc(o =>
+            {
+                o.ModelBindingMessageProvider.SetAttemptedValueIsInvalidAccessor((x, y) => "O Valor preenchido é inválido para este campo!");
+                o.ModelBindingMessageProvider.SetMissingBindRequiredValueAccessor(x => "Este campo precisa ser preenchido!");
+                o.ModelBindingMessageProvider.SetMissingKeyOrValueAccessor(() => "Este campo precisa ser preenchido!");
+                o.ModelBindingMessageProvider.SetMissingRequestBodyRequiredValueAccessor(() => "É necessário que o body na requisição não esteja vazio!");
+                o.ModelBindingMessageProvider.SetNonPropertyAttemptedValueIsInvalidAccessor((x) => "O valor preenchido é inválido para este campo!");
+                o.ModelBindingMessageProvider.SetNonPropertyUnknownValueIsInvalidAccessor(() => "O Valor preenchido é inválido para este campo!");
+                o.ModelBindingMessageProvider.SetNonPropertyValueMustBeANumberAccessor(() => "Este campo deve ser númerico!");
+                o.ModelBindingMessageProvider.SetUnknownValueIsInvalidAccessor((x) => "O Valor preenchido é inválido para este campo!");
+                o.ModelBindingMessageProvider.SetValueIsInvalidAccessor((x) => "O Valor preenchido é inválido para este campo!");
+                o.ModelBindingMessageProvider.SetValueMustBeANumberAccessor((x) => "Este campo deve ser númerico!");
+                o.ModelBindingMessageProvider.SetValueMustNotBeNullAccessor((x) => "Este campo precisa ser preenchido!");
+
+            });
+
+            return services;
+        }
+    }
+}
